78b9fa18fd440c15f97209ba7ad5c3d46a7c77ef
Frida Arcadia <frida.arcadialv@gmail.com>
2025-02-21 21:24:44 -0600

    [UPDATE] Laboratorio 6 & answer questions

--- js/myScriptLab6.js
+++ js/myScriptLab6.js
@@ -4,7 +4,6 @@ function changeVista(viewId) {
   const selectedView = document.getElementById(viewId);
   selectedView.classList.add("active");
 }
-
 const inputField = document.getElementById("password");
 const feedback = document.getElementById("feedback");
 
@@ -16,7 +15,11 @@ inputField.addEventListener("keyup", function () {
     errorMessage.push("You must include a number!");
   }
 
-  if (!/[!.-_]/.test(password)) {
+  if (!/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/.test(password)) {
+    errorMessage.push("You must include Capital letters!");
+  }
+
+  if (!/[!@#$%^&*(),.?":{}|<>_-]/.test(password)) {
     errorMessage.push("You must include a symbol!");
   }
 
@@ -25,10 +28,53 @@ inputField.addEventListener("keyup", function () {
   }
 
   if (errorMessage.length > 0) {
-    feedback.textContent = errorMessage.join("\n");
+    feedback.innerHTML = errorMessage.join("<br>");
     feedback.className = "error";
   } else {
-    feedback.textContent = "Valid Password";
+    feedback.textContent = "Valid Password. Confirm your password below";
     feedback.className = "valid";
   }
 });
+
+const inputConfirmation = document.getElementById("confirm-pass");
+const confirmationFeedback = document.getElementById("confirmationFeedback");
+
+inputConfirmation.addEventListener("keyup", function () {
+  const confirmation = inputConfirmation.value;
+  const password = inputField.value;
+
+  let confirmationMessage = [];
+
+  if (confirmation != password) {
+    confirmationMessage.push("Passwords do not match");
+  }
+
+  if (confirmationMessage.length > 0) {
+    confirmationFeedback.innerHTML = confirmationMessage.join("<br>");
+    confirmationFeedback.className = "error";
+  } else {
+    confirmationFeedback.textContent = "Passwords match";
+    confirmationFeedback.className = "valid";
+  }
+});
+
+function enableButton() {
+  const finalPassword = inputField.value;
+  const finalConfirmPassword = inputConfirmation.value;
+  console.log(finalPassword);
+  console.log(finalConfirmPassword);
+  if (finalPassword === finalConfirmPassword) {
+    second_button.disabled = false;
+  } else {
+    second_button.disabled = true;
+  }
+}
+
+function changeVista2(viewId) {
+  const views = document.querySelectorAll(".view");
+  views.forEach((view) => view.classList.remove("active"));
+  const selectedView = document.getElementById(viewId);
+  selectedView.classList.add("active");
+}
+
+inputConfirmation.addEventListener("keyup", enableButton);
