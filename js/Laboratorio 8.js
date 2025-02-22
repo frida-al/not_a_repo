5012abb39384ada0dab26868294306538634d2ae
Frida Arcadia <frida.arcadialv@gmail.com>
2025-02-22 15:04:24 -0600

    [ADD] third exercise, Laboratorio 8

--- js/Laboratorio 8.js
+++ js/Laboratorio 8.js
@@ -14,3 +14,27 @@ console.log(promedio(miArreglo));
 const file_system = require("fs");
 const stringTexto = "Hola, para el laboratorio 8";
 file_system.writeFileSync("Labo8.txt", stringTexto);
+
+const miArreglo2 = [
+  "localization",
+  "internationalization",
+  "word",
+  "pneumonoultramicroscopicsilicovolcanoconiosis",
+];
+
+function wordscounter(array) {
+  const arreglo = array;
+  const size = arreglo.length;
+  for (let i = 0; i < size; i++) {
+    let string = arreglo[i];
+    if (string.length > 10) {
+      const first = string[0];
+      const last = string[string.length - 1];
+      const between = string.length - 2;
+      console.log(first + between + last);
+    } else {
+      console.log(string);
+    }
+  }
+}
+console.log(wordscounter(miArreglo2));
